de35e7fea5e022c6ffba53863c5768f8f2c617fc
hayabusa <10593623+m-hayabusa@users.noreply.github.com>
2023-04-01 11:59:46 +0900

    フォーマッタの設定

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
-#include <WiFi.h>
 #include <ESPmDNS.h>
-#include <WebServer.h>
 #include <Preferences.h>
 #include <Update.h>
+#include <WebServer.h>
+#include <WiFi.h>
 
 #define PIN_PWRLED_MB 5
 #define PIN_HDDLED_MB 4
@@ -20,12 +20,7 @@ bool pwrLedState = false;
 bool hddLedState = false;
 bool powerctl = false;
 
-enum class PowerState
-{
-  Off,
-  On,
-  Sleep
-};
+enum class PowerState { Off, On, Sleep };
 PowerState powerState = PowerState::Off;
 Preferences Config;
 
@@ -36,8 +31,7 @@ void LedPwm(void *pvParameters);
 void PowerButton(void *pvParameters);
 void SerialInput(void *pvParameters);
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   Serial.println("hello");
 
@@ -63,22 +57,17 @@ void setup()
   Serial.println("nya");
 }
 
-void SerialInput(void *pvParameters)
-{
+void SerialInput(void *pvParameters) {
   String raw = "";
   Serial.setTimeout(1);
-  while (1)
-  {
+  while (1) {
     delay(10);
 
-    if (Serial.available() > 0)
-    {
-      while (Serial.available() > 0)
-      {
+    if (Serial.available() > 0) {
+      while (Serial.available() > 0) {
         char c = Serial.read();
 
-        if (c == 0x08)
-          raw[raw.length() - 1] = 0;
+        if (c == 0x08) raw[raw.length() - 1] = 0;
 
         Serial.print(c);
         raw += c;
@@ -86,47 +75,35 @@ void SerialInput(void *pvParameters)
 
       const int nl = raw.indexOf('\n');
 
-      if (nl != -1)
-      {
+      if (nl != -1) {
         const String line = raw.substring(0, nl);
         Serial.println(line);
         raw = raw.substring(nl + 1);
 
         const int splitter = line.indexOf(':');
 
-        if (splitter != -1)
-        {
+        if (splitter != -1) {
           String name = line.substring(0, splitter);
           String data = line.substring(splitter + 1);
           Serial.println(name + " : " + data);
-          if (name == "WIFI.SSID" || name == "WIFI.PASSWORD" || name == "WEB.PASSWORD" || name == "WEB.HOSTNAME")
-          {
+          if (name == "WIFI.SSID" || name == "WIFI.PASSWORD" || name == "WEB.PASSWORD" || name == "WEB.HOSTNAME") {
             Config.putString(name.c_str(), data);
             Serial.println(name + ":" + Config.getString(name.c_str()) + ", saved");
-            if (name == "WIFI.SSID" || name == "WIFI.PASSWORD")
-            {
+            if (name == "WIFI.SSID" || name == "WIFI.PASSWORD") {
               WiFi.disconnect();
               WiFi.begin(Config.getString("WIFI.SSID").c_str(), Config.getString("WIFI.PASSWORD").c_str());
-            }
-            else if (name == "WEB.HOSTNAME")
-            {
+            } else if (name == "WEB.HOSTNAME") {
               MDNS.end();
               delay(100);
               MDNS.begin(Config.getString("WEB.HOSTNAME", "RemotePowerButton").c_str());
               MDNS.addService("http", "tcp", 80);
             }
-          }
-          else
-          {
+          } else {
             Serial.println(name + " is not in list");
           }
-        }
-        else if (line == "WIFI.SSID" || line == "WIFI.PASSWORD" || line == "WEB.PASSWORD" || line == "WEB.HOSTNAME")
-        {
+        } else if (line == "WIFI.SSID" || line == "WIFI.PASSWORD" || line == "WEB.PASSWORD" || line == "WEB.HOSTNAME") {
           Serial.println(line + ":" + Config.getString(line.c_str()));
-        }
-        else
-        {
+        } else {
           Serial.println("> KEY:VALUE");
         }
       }
@@ -134,8 +111,7 @@ void SerialInput(void *pvParameters)
   }
 }
 
-void LedPwm(void *pvParameters)
-{
+void LedPwm(void *pvParameters) {
   ledcSetup(0, 12800, 8);
   ledcAttachPin(PIN_PWRLED_CASE, 0);
   ledcSetup(1, 12800, 8);
@@ -144,8 +120,7 @@ void LedPwm(void *pvParameters)
   int pwrlevel = 0;
   int hddlevel = 0;
 
-  while (1)
-  {
+  while (1) {
     if (pwrLedState)
       pwrlevel += 24;
     else
@@ -175,37 +150,32 @@ void LedPwm(void *pvParameters)
   }
 }
 
-void PwrLedControl(void *pvParameters)
-{
+void PwrLedControl(void *pvParameters) {
   Serial.println("ledControl");
-  while (1)
-  {
-    switch (powerState)
-    {
-    case PowerState::On:
-      pwrLedState = true;
-      delay(2900);
-      break;
+  while (1) {
+    switch (powerState) {
+      case PowerState::On:
+        pwrLedState = true;
+        delay(2900);
+        break;
 
-    case PowerState::Sleep:
-      pwrLedState = true;
-      delay(1000);
-      pwrLedState = false;
-      delay(1000);
-      break;
+      case PowerState::Sleep:
+        pwrLedState = true;
+        delay(1000);
+        pwrLedState = false;
+        delay(1000);
+        break;
 
-    case PowerState::Off:
-      pwrLedState = true;
-      delay(100);
-      pwrLedState = false;
-      delay(2900);
-      break;
+      case PowerState::Off:
+        pwrLedState = true;
+        delay(100);
+        pwrLedState = false;
+        delay(2900);
+        break;
     }
-    if (!WiFi.isConnected())
-    {
+    if (!WiFi.isConnected()) {
       delay(200);
-      for (int i = 0; i < 3; i++)
-      {
+      for (int i = 0; i < 3; i++) {
         pwrLedState = true;
         delay(100);
         pwrLedState = false;
@@ -215,42 +185,32 @@ void PwrLedControl(void *pvParameters)
   }
 }
 
-void PowerButton(void *pvParameters)
-{
-  while (1)
-  {
-    if (!powerctl)
-    {
+void PowerButton(void *pvParameters) {
+  while (1) {
+    if (!powerctl) {
       digitalWrite(PIN_PWRBTN_MB, !digitalRead(PIN_PWRBTN_CASE));
     }
     delay(10);
   }
 }
 
-void PwrControl(void *pvParameters)
-{
+void PwrControl(void *pvParameters) {
   Serial.println("PwrControl");
   int counter = 0;
 
-  while (1)
-  {
+  while (1) {
     if (!digitalRead(PIN_PWRLED_MB))
       counter++;
     else
       counter--;
 
-    if (counter <= 0)
-    {
+    if (counter <= 0) {
       powerState = PowerState::Off;
       counter = 1;
-    }
-    else if (counter >= 15)
-    {
+    } else if (counter >= 15) {
       powerState = PowerState::On;
       counter = 14;
-    }
-    else
-    {
+    } else {
       powerState = PowerState::Sleep;
     }
 
@@ -258,8 +218,7 @@ void PwrControl(void *pvParameters)
   }
 }
 
-void httpServer(void *pvParameters)
-{
+void httpServer(void *pvParameters) {
   Serial.println("httpServer");
 
   WiFi.mode(WIFI_MODE_STA);
@@ -268,17 +227,16 @@ void httpServer(void *pvParameters)
 
   WebServer *server = new WebServer(80);
 
-  server->on("/", [&server]()
-             {
+  server->on("/", [&server]() {
     if (server->method() != HTTPMethod::HTTP_GET) {
       server->send(405, "text/plain", "GET Only");
       return;
     }
 #include "index.html.cpp"
-    server->send(200, "text/html",INDEX_HTML); });
+    server->send(200, "text/html", INDEX_HTML);
+  });
 
-  server->on("/state", [&server]()
-             {
+  server->on("/state", [&server]() {
     if (server->method() != HTTPMethod::HTTP_GET) {
       server->send(405, "text/plain", "GET Only");
       return;
@@ -297,10 +255,10 @@ void httpServer(void *pvParameters)
     }
     result += "\"}";
 
-    server->send(200, "application/json", result); });
+    server->send(200, "application/json", result);
+  });
 
-  server->on("/name", [&server]()
-             {
+  server->on("/name", [&server]() {
     if (server->method() != HTTPMethod::HTTP_GET) {
       server->send(405, "text/plain", "GET or POST");
     }
@@ -308,10 +266,10 @@ void httpServer(void *pvParameters)
     result += Config.getString("WEB.HOSTNAME");
     result += "\"}";
     server->send(200, "application/json", result);
-    return; });
+    return;
+  });
 
-  server->on("/on", [&server]()
-             {
+  server->on("/on", [&server]() {
     if (server->method() != HTTPMethod::HTTP_POST) {
       server->send(405, "text/plain", "POST Only");
       return;
@@ -329,10 +287,10 @@ void httpServer(void *pvParameters)
       powerctl = false;
     } else {
       server->send(409, "text/plain", "already on");
-    } });
+    }
+  });
 
-  server->on("/off", [&server]()
-             {
+  server->on("/off", [&server]() {
     if (server->method() != HTTPMethod::HTTP_POST) {
       server->send(405, "text/plain", "POST Only");
       return;
@@ -350,10 +308,10 @@ void httpServer(void *pvParameters)
       powerctl = false;
     } else {
       server->send(409, "text/plain", "already off");
-    } });
+    }
+  });
 
-  server->on("/force-off", [&server]()
-             {
+  server->on("/force-off", [&server]() {
     if (server->method() != HTTPMethod::HTTP_POST) {
       server->send(405, "text/plain", "POST Only");
       return;
@@ -371,75 +329,64 @@ void httpServer(void *pvParameters)
       powerctl = false;
     } else {
       server->send(409, "text/plain", "already off");
-    } });
+    }
+  });
 
-  server->on("/update", HTTP_GET, [&server]()
-             {
+  server->on("/update", HTTP_GET, [&server]() {
 #include "update.html.cpp"
-        server->send(200, "text/html", UPDATE_HTML); });
+    server->send(200, "text/html", UPDATE_HTML);
+  });
   // OTA Update
   server->on(
-      "/firmware", HTTP_POST, [&server]()
-      {
-    server->sendHeader("Connection", "close");
-    if (Update.hasError())
-    {
-      server->send(500, "text/plain", Update.errorString());
-    }
-    else
-    {
-      server->send(200, "text/plain", "update done");
-      ESP.restart();
-    } },
-      [&server]()
-      {
+      "/firmware", HTTP_POST,
+      [&server]() {
+        server->sendHeader("Connection", "close");
+        if (Update.hasError()) {
+          server->send(500, "text/plain", Update.errorString());
+        } else {
+          server->send(200, "text/plain", "update done");
+          ESP.restart();
+        }
+      },
+      [&server]() {
         HTTPUpload &upload = server->upload();
 
-        switch (upload.status)
-        {
-        case UPLOAD_FILE_START:
-          Update.begin();
-          break;
+        switch (upload.status) {
+          case UPLOAD_FILE_START:
+            Update.begin();
+            break;
 
-        case UPLOAD_FILE_WRITE:
-          Update.write(upload.buf, upload.currentSize);
-          break;
+          case UPLOAD_FILE_WRITE:
+            Update.write(upload.buf, upload.currentSize);
+            break;
 
-        case UPLOAD_FILE_END:
-          if (Update.end(true))
-            Serial.printf("written %u / Restart", upload.totalSize);
-          else
-            Update.printError(Serial);
-          break;
+          case UPLOAD_FILE_END:
+            if (Update.end(true))
+              Serial.printf("written %u / Restart", upload.totalSize);
+            else
+              Update.printError(Serial);
+            break;
 
-        case UPLOAD_FILE_ABORTED:
-          Update.abort();
-          break;
+          case UPLOAD_FILE_ABORTED:
+            Update.abort();
+            break;
         }
       });
-  server->on("/firmware", HTTP_GET, [&server]()
-             { server->send(405, "Method Not Allowed"); });
+  server->on("/firmware", HTTP_GET, [&server]() { server->send(405, "Method Not Allowed"); });
 
   bool active = false;
 
-  while (1)
-  {
-    if (WiFi.isConnected())
-    {
-      if (active)
-      {
+  while (1) {
+    if (WiFi.isConnected()) {
+      if (active) {
         server->handleClient();
-      }
-      else
-      {
+      } else {
         server->begin();
         MDNS.begin(Config.getString("WEB.HOSTNAME", "RemotePowerButton").c_str());
         MDNS.addService("http", "tcp", 80);
         active = true;
       }
-    }
-    else if (active)
-    {
+    } else if (active) {
       server->close();
       MDNS.end();
       active = false;
@@ -449,6 +396,4 @@ void httpServer(void *pvParameters)
   }
 }
 
-void loop()
-{
-}
+void loop() {}
